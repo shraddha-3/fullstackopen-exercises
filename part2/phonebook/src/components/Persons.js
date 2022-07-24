057dd425d2b787a79693a22932efe42250cb380e
shraddha-3 <onlypeaceinlife@gmail.com>
2022-07-24 23:01:11 +0530

    Add part 2.11 phonebook step6

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -3,7 +3,7 @@ import Person from './Person'
 
 const Persons = ({persons}) => {
     return(
-        <ul>{persons.map(person => <Person person={person}/>)}</ul>
+        <ul>{persons.map(person => <Person key={person.id} person={person}/>)}</ul>
     )
 }
 
