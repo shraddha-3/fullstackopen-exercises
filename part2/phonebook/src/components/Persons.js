4ec9194ef63aa9f7fafc3b7796717dc07b8333e6
shraddha-3 <onlypeaceinlife@gmail.com>
2023-01-03 15:07:43 +0530

    Change: Add part 3.9-3.11 phonebook-backend step 9-11

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -2,9 +2,17 @@ import React from 'react'
 import Person from './Person'
 
 const Persons = ({deletePerson, persons}) => {
-    return(
+    console.log(persons.length)
+    if(persons.length > 0)
+    {
+        
+        return(
         <ul>{persons.map(person => <Person key={person.id} name={person.name} number={person.number} deletePerson={deletePerson} id={person.id}/>)}</ul>
     )
+    }
+
+    return null
+    
 }
 
 export default Persons
